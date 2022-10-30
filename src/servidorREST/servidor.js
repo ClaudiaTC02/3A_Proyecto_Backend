dcc01f84c1f79a80dd01207f375dbb9712cd8055
Claudia <claudiatcruz2002@gmail.com>
2022-10-30 15:26:53 +0100

    Creo la base del servidor

---
+++ src/servidorREST/servidor.js
@@ -0,0 +1,66 @@
+/**
+ *
+ * NOMBRE: servidor.js
+ * AUTORA: Claudia Torres Cruz
+ * FECHA: 30/10/2022
+ * DESCRIPCIÓN: Este fichero contiene las características del servidor REST
+ */
+//------------------------------------------------------------------------------
+const express = require("express");
+const Logica = require("../logica/logica.js");
+//------------------------------------------------------------------------------
+/**
+ * @param bbdd
+ * @param usuario
+ * @param host
+ * @param puerto
+ * @param dialecto
+ * @return objeto Logica
+ * Diseño: bbdd: texto, usuario: texto, host: texto, puerto: Z, dialecto: texto --> cargarLogica() --> Logica
+ */
+//------------------------------------------------------------------------------
+function cargarLogica(bbdd, usuario, host, puerto, dialecto) {
+  return new Logica(bbdd, usuario, host, puerto, dialecto);
+}
+//------------------------------------------------------------------------------
+/**
+ * @brief esta función se encarga de arrancar el servidor rest adecuadamente
+ * Diseño: --> main() -->
+ */
+//------------------------------------------------------------------------------
+async function main() {
+  var logica = cargarLogica("gti3a_proyecto", "root", "localhost", 3306, "mysql");
+  var reglas = require("./reglasREST.js");
+  //var medicion = logica.cargarModeloMedicion();
+  //creo el servidor
+  var servidorExpress = express();
+  // para poder acceder a la carga de la peticion
+  servidorExpress.use(express.json());
+  servidorExpress.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+    );
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, DELETE"
+    );
+    res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+  });
+  //Cargo las reglas rest
+  reglas.cargar(servidorExpress, logica);
+  // arranco el servidor
+  var servicio = servidorExpress.listen(8080, function () {
+    console.log("Servidor REST conectado en el puerto 8080");
+  });
+  // capturo control-c para cerrar el servicio ordenadamente
+  process.on("SIGINT", function () {
+    console.log(" terminando servicio... ");
+    servicio.close();
+  });
+} // main()
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+main();
