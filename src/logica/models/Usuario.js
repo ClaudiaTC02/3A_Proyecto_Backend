4f791efba42efd8db3f25a338a485cc5472ea676
Claudia <claudiatcruz2002@gmail.com>
2022-11-17 13:16:43 +0100

    Modelo de usuario actualizado

--- src/logica/models/Usuario.js
+++ src/logica/models/Usuario.js
@@ -17,6 +17,7 @@ module.exports = (sequelize, DataTypes) => {
     Nombre: DataTypes.STRING,
     Contrasena: DataTypes.STRING,
     Correo: DataTypes.STRING,
+    EsAdmin: DataTypes.BOOLEAN
   }, {
     sequelize,
     modelName: 'Usuario',
