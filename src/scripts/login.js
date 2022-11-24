82c08d49fe066c41780a128095f3ac18e0cc3c6a
DavidLopez <davidlopez.gandia@gmail.com>
2022-11-24 21:28:04 +0100

    Login web funcionando

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,25 +1,33 @@
 
+
 const PUERTO_IP = "http://localhost:8080"
 
-function buscarUsuario(correo, contrasena, cb){
-    fetch(PUERTO_IP + "/buscarUsuario?Correo="+correo+"&Contrasena="+contrasena, {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-      })
-        .catch((err) =>
-          console.log("No se pudo recoger los datos del usuario" + err)
-        )
-        .then((res) => res.json())
-        .then((resJSON) => {
-          cb(resJSON);
-        });
-}
+
+
 
 function login(){
     const correo= document.getElementById('correo').value;
     const contrasena = document.getElementById('contrasenya').value;
     const boton = document.getElementById('boton_login');
     console.log(correo);
-    buscarUsuario(correo, contrasena, funcion(res))
-        console.log(res);
+    verificarUsuario(correo,contrasena,function (res) {
+      console.log(res.id);
+      window.location.href = "admin.html?"+ res.id;
+    });
+   
+    
 }
+
+function verificarUsuario(correo,contrasena,cb) {
+  fetch(PUERTO_IP + "/verificarUsuario?Correo="+correo+"&Contrasena="+contrasena, {
+      method: "GET",
+      headers: {
+          "Content-Type": "application/json",
+      }
+  })
+      .catch((err) => console.log('Fetch failed ' + err))
+      .then((res) => res.json())
+      .then(data => {
+          cb(data)
+      })
+} // ()
