745c8ae0817c0581d8e4fe88b7769ea0a7ffca9a
Jordi272 <jordidomenech27@gmail.com>
2023-01-02 11:08:01 +0100

    correcion diseños

---
+++ src/logica/test/frontendTest.js
@@ -0,0 +1,34 @@
+const assert = require('assert');
+
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+
+(async function boton_call_to_action_check() {
+  console.log("inciio");
+     let driver = await new Builder().forBrowser('chrome').build();
+     try {
+    // Primero, navegamos a la página que queremos probar
+    await driver.get('file:///C:/Users/jordi/Documents/GitHub/GTI_3A_Backend/src/ux/index.html');
+
+    // Luego, buscamos el botón que queremos probar
+    let button = await driver.findElement(By.id('boton_call_to_action'));
+    //console.log("ha llegado al elemento");
+    // Verificamos que el botón esté habilitado y tenga el texto correcto
+    assert(await button.isEnabled());
+
+    // Hacemos clic en el botón
+    await button.click();
+    //console.log("ha pulsado el boton");
+    // Verificamos que se haya realizado la acción esperada (por ejemplo,
+    // que se haya redirigido a otra página o que se haya mostrado un mensaje)
+    let url = await driver.getCurrentUrl();
+    assert(url.startsWith('file:///C:/Users/jordi/Documents/GitHub/GTI_3A_Backend/src/ux/mapas.html'));
+    console.log("Funciona el test 1, comprobación call to action")
+
+  } finally {
+    // Asegúrate de cerrar el navegador cuando hayas terminado
+    await driver.quit();
+  }
+})();
+
+
